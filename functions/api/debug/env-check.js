8111d1bd86f561b536659e0fba315565a76aa5d3
Jason Lee <applelight1993@gmail.com>
2025-11-09 17:20:30 -0500

    payment setup

---
+++ functions/api/debug/env-check.js
@@ -0,0 +1,15 @@
+export const onRequest = async ({ env }) => {
+  const present = {
+    STRIPE_SECRET_KEY: !!env.STRIPE_SECRET_KEY,
+    STRIPE_PRICE_ID: !!env.STRIPE_PRICE_ID,
+    STRIPE_SUCCESS_URL: !!env.STRIPE_SUCCESS_URL,
+    STRIPE_CANCEL_URL: !!env.STRIPE_CANCEL_URL,
+    ELARIN_DB: !!(env.ELARIN_DB || env.elarin_db),
+  };
+  return new Response(JSON.stringify(present, null, 2), {
+    headers: {
+      "content-type": "application/json",
+      "cache-control": "no-store",
+    },
+  });
+};
